1b8e0d5192ac20e44bb725cf94ae0def32e218c9
jose.sanchez <jose.sanchez@Arturo-THINK.miraflores.gob.pe>
2016-02-16 16:32:14 -0500

    Actualizado al 16-02-2016

--- AppMobile/instrucciones.sql
+++ AppMobile/instrucciones.sql
@@ -4,7 +4,7 @@
 	Our Road Closure Registration tool that automatically generates a Waze-consumable feed is a java-based application that has been developed using the following frameworks:
 	Backend:
 	-Spring 4
-	-Myibatis
+	-Mybatis
 	Frontend
 	-ExtJs
 	-Jquery
@@ -14,25 +14,43 @@
 	We used Eclipse as IDE for the development (version Mars). 
 	However there is no problem with using other IDES like netbeans for instance, since this application has been developed conforming the MAVEN standards.
 
+	The tool allows to zoom the map in to specific locations according to our cadastral Database. It does this by querying an external service that provides the spatial coordinates and a predefined polygon. The service is not included in the code we are sharing, so it is the duty of the development team to produce such a service. In order to avoid having to change the existing code, the service to be developed must produce a response with the following JSON structure: 
+	{
+		total: 0,
+		data: [
+			{
+			tipo: "AVENIDAS",
+			toponimia: "AV. JOSE A. LARCO",
+			cdra: "4",
+			gid: 478,
+			punto: "-77.0290672344261 -12.121864951297",
+			poligono: "-77.0290399232 -12.1214598600923,-77.029094545835 -12.1222700424757"
+			}
+		]
+	}
 
--- ESPA脩OL:
-	Descripci贸n de la Aplicacion:
+	We provide an example link here:
+	http://digital.miraflores.gob.pe:8080/MirafloresV1/obtenergeoreferenciaxvia.muni?query=larco%204&start=0&limit=100
+	The result provided in this example zooms in to the polygon in the map that corresponds to the 4th block of Larco Avenue in Miraflores. 
+
+-- ESPA袿L:
+	Descripci髇 de la Aplicacion:
 	La herramienta del formulario de cierre de calles es una aplicacion desarrollada en java web con los siguientes frameworks de desarrollo:
 	Backend:
 	-Spring 4
-	-Myibatis
+	-Mybatis
 	Frontend
 	-ExtJs
 	-Jquery
 	-Bootstrap 3
-	La base de datos en la cual se desarrollo la aplicaci贸n es DB2.
+	La base de datos en la cual se desarrollo la aplicaci髇 es DB2.
 	La aplicacion esta montada en servidor tomcat 7, con JDK 7.
-	El IDE de desarrollo en el cual se implemento la aplicaci贸n fue eclipse en la version Mars, 
-	sin embargo no existe ningun inconveniente en ejecutarlo en otros IDES como netbeans por ejemplo, debido a que dicha aplicaci贸n
+	El IDE de desarrollo en el cual se implemento la aplicaci髇 fue eclipse en la version Mars, 
+	sin embargo no existe ningun inconveniente en ejecutarlo en otros IDES como netbeans por ejemplo, debido a que dicha aplicaci髇
 	esta soportada bajo el estandares de MAVEN.
 	NOTAS: 
 	
-	1. El servicio que proporciona informaci贸n para la aproximaci贸n de calles, se encuentra ubicado en el siguiente enlace como ejemplo:
+	1. El servicio que proporciona informaci髇 para la aproximaci髇 de calles, se encuentra ubicado en el siguiente enlace como ejemplo:
 	http://digital.miraflores.gob.pe:8080/MirafloresV1/obtenergeoreferenciaxvia.muni?query=larco 4&start=0&limit=100
 	
 	el cual traera la siguiente respuesta en formato json:
@@ -50,11 +68,10 @@
 		]
 	}
 	
-	El ejemplo nos aproxima a la cuadra 4 de la av. larco en miraflores, con la geolocalizacion correspondiente a esta direcci贸n
+	El ejemplo nos aproxima a la cuadra 4 de la av. larco en miraflores, con la geolocalizacion correspondiente a esta direcci髇
 	
-	2. En el formulario de registro existe una casilla de verificacion llamada visible, esta opci贸n nos permite ocultar aquellos 
+	2. En el formulario de registro existe una casilla de verificacion llamada visible, esta opci髇 nos permite ocultar aquellos 
 	registros que no deseemos que se muestren en el fit de waze.
-
 */
 -----------------------------------------------------------------------
 --SCRIPT DE TABLAS
@@ -169,7 +186,7 @@ CREATE UNIQUE INDEX GEOMGEOMETRIA ON GEOMGEOMETRIA
 --INSERTS TABLA CATALOGO
 -----------------------------------------------------------------------
 INSERT INTO CTLMELECGO (IDE_ELEMENTO,IDE_GRUPO,COD_CODIGO,DES_NOMBRE,CLV_ABREVIAR,IDE_ELEMENTO1,IND_ESTADO,FEC_FECHA_CREACION,REFERENCIA,REFERENCIA1,REFERENCIA2) VALUES (1441,152,'1','PUNTO','',1441,'1',{d '2012-10-04'},'','','');
-INSERT INTO CTLMELECGO (IDE_ELEMENTO,IDE_GRUPO,COD_CODIGO,DES_NOMBRE,CLV_ABREVIAR,IDE_ELEMENTO1,IND_ESTADO,FEC_FECHA_CREACION,REFERENCIA,REFERENCIA1,REFERENCIA2) VALUES (8748,323,'1','Construcci贸n o Incidente','',null,'1',{d '2015-10-26'},'','','');
+INSERT INTO CTLMELECGO (IDE_ELEMENTO,IDE_GRUPO,COD_CODIGO,DES_NOMBRE,CLV_ABREVIAR,IDE_ELEMENTO1,IND_ESTADO,FEC_FECHA_CREACION,REFERENCIA,REFERENCIA1,REFERENCIA2) VALUES (8748,323,'1','Construcci髇 o Incidente','',null,'1',{d '2015-10-26'},'','','');
 INSERT INTO CTLMELECGO (IDE_ELEMENTO,IDE_GRUPO,COD_CODIGO,DES_NOMBRE,CLV_ABREVIAR,IDE_ELEMENTO1,IND_ESTADO,FEC_FECHA_CREACION,REFERENCIA,REFERENCIA1,REFERENCIA2) VALUES (8749,323,'2','Via Cerrada','',null,'1',{d '2015-10-26'},'','','');
 INSERT INTO CTLMELECGO (IDE_ELEMENTO,IDE_GRUPO,COD_CODIGO,DES_NOMBRE,CLV_ABREVIAR,IDE_ELEMENTO1,IND_ESTADO,FEC_FECHA_CREACION,REFERENCIA,REFERENCIA1,REFERENCIA2) VALUES (8750,42,'30','TABLA_MAPA_INCIDENCIAS','',null,'1',{d '2015-10-27'},'','','');
 INSERT INTO CTLMELECGO (IDE_ELEMENTO,IDE_GRUPO,COD_CODIGO,DES_NOMBRE,CLV_ABREVIAR,IDE_ELEMENTO1,IND_ESTADO,FEC_FECHA_CREACION,REFERENCIA,REFERENCIA1,REFERENCIA2) VALUES (8765,39,'25','VOXIVA_WS','',null,'1',{d '2015-11-27'},'','','');
@@ -177,4 +194,3 @@ INSERT INTO CTLMELECGO (IDE_ELEMENTO,IDE_GRUPO,COD_CODIGO,DES_NOMBRE,CLV_ABREVIA
 -----------------------------------------------------------------------
 --END INSERTS TABLA CATALOGO
 -----------------------------------------------------------------------
-
